3088793499b81b5755852d8978451bb54c4956c9
mikejunt <mikejunt@gmail.com>
2020-03-24 09:57:52 -0500

    brings in basic Auth0 config

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using baseballapi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 
 namespace baseballapi
@@ -28,6 +29,15 @@ namespace baseballapi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options => 
+            {
+                options.Authority = $"https://{Configuration["Auth0:Domain"]}/";
+                options.Audience = Configuration["Auth0:Audience"];
+            });
             services.AddControllers();
             services.AddDbContext<baseballdbContext>(options =>
                 options.UseNpgsql(Configuration["ConnectionString"]));
